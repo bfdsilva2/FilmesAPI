688494b6f5bd75ed6c8bae44d86e2cc07c3282dd
bfdsi <bfdsi@LAPTOP-56B8E63F>
2023-05-30 09:18:13 -0300

    Class2

---
+++ FilmesAPI/Controllers/MovieController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[Coontroller]")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+        Console.WriteLine(movie.Title);
+        Console.WriteLine(movie.Duration);
+        Console.WriteLine(movie.Genre);
+    }
+}
---
+++ FilmesAPI/Models/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace FilmesAPI.Models;
+
+public class Movie
+{
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public int Duration { get; set; }
+}
